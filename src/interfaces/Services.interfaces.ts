d5f87aa6b87cc17db3a99db022e2577f241f61c8
Ismael Correia <ismaelavilacorreia@gmail.com>
2022-05-17 15:51:38 -0300

    feat: cria service migration

---
+++ src/interfaces/Services.interfaces.ts
@@ -0,0 +1,12 @@
+export interface IService {
+  id: string;
+  name: string;
+  price: number;
+  category: string;
+}
+
+export interface IServiceCreate {
+  name: string;
+  price: number;
+  category: string;
+}
---
+++ src/services/services/CreateService.service.ts
@@ -0,0 +1,30 @@
+import { AppDataSource } from "../../data-source";
+import Service from "../../entities/Services";
+import AppError from "../../errors/AppError";
+import { IServiceCreate } from "../../interfaces/Services.interfaces";
+
+const createServiceService = async ({
+  name,
+  price,
+  category,
+}: IServiceCreate) => {
+  const serviceRepository = AppDataSource.getRepository(Service);
+
+  const serviceExistance = await serviceRepository.findOne({ where: { name } });
+
+  if (serviceExistance) {
+    throw new AppError("Serviço já cadastrado.");
+  }
+
+  const service = new Service();
+  service.name = name;
+  service.price = price;
+  service.category = category;
+
+  serviceRepository.create(service);
+  await serviceRepository.save(service);
+
+  return service;
+};
+
+export default createServiceService;
