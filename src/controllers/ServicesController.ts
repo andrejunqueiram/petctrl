33691a3bb123c05accb545ab81acc5f87d47c8cd
Ismael Correia <ismaelavilacorreia@gmail.com>
2022-05-17 14:14:15 -0300

    feat: cria service e dependencias

---
+++ src/controllers/ServicesController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+export default class ServicesController {
+  static async store(req: Request, res: Response) {}
+  static async index(req: Request, res: Response) {}
+  static async update(req: Request, res: Response) {}
+  static async delete(req: Request, res: Response) {}
+}
