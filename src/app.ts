6dfb3477ea4251a628032fd315fe7646715a369f
Andre Junqueira <andrejmelgaco@gmail.com>
2022-05-17 11:59:46 -0300

    feat: config e estrutura atualizadas

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,20 @@ import express from "express";
 import "express-async-errors";
 
 import "dotenv/config";
+import ErrorMiddleware from "./middlewares/error.middleware";
 
-// import routes from "./routes";
+import routes from "./routes";
 // import AppError from "./errors/AppError";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/teste", (req, res) => {
-  res.send("Hello");
+app.use(ErrorMiddleware.execute);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
-// app.use(routes);
+app.use(routes);
 
 export default app;
---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import AppError from "../errors/AppError";
+
+export default class ErrorMiddleware {
+  static execute(
+    err: Error,
+    request: Request,
+    response: Response,
+    _: NextFunction
+  ) {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    console.log(err);
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  }
+}
