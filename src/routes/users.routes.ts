75724bd050f65a1c10f73d7e80e22e029d53d094
Andre Junqueira <andrejmelgaco@gmail.com>
2022-05-17 19:54:15 -0300

    fix: arruma nome Controller

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import UserController from "../controllers/user.controller";
+import UserController from "../controllers/User.controller";
 
 const userRoutes = Router();
 
