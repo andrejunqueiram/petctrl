d0b11498420e1cd64a1e71fb30b6c86ea696f193
Andre Junqueira <andrejmelgaco@gmail.com>
2022-05-20 17:33:27 -0300

    feat: deploy

--- src/middlewares/checkAdm.middleware.ts
+++ src/middlewares/checkAdm.middleware.ts
@@ -1,33 +1,33 @@
-import { NextFunction, Request, Response } from "express";
-import { AppDataSource } from "../data-source";
-import User from "../entities/User";
-import AppError from "../errors/AppError";
+// import { NextFunction, Request, Response } from "express";
+// import { AppDataSource } from "../data-source";
+// import User from "../entities/User";
+// import AppError from "../errors/AppError";
 
-const checkAdmCheckMiddleware = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const { id } = req.user;
-  const userRepository = AppDataSource.getRepository(User);
-  const user = await userRepository.findOne({
-    where: {
-      id,
-    },
-  });
+// const checkAdmCheckMiddleware = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   const { id } = req.user;
+//   const userRepository = AppDataSource.getRepository(User);
+//   const user = await userRepository.findOne({
+//     where: {
+//       id,
+//     },
+//   });
 
-  if (!user) {
-    throw new AppError("Usuário não encontrado", 404);
-  }
+//   if (!user) {
+//     throw new AppError("Usuário não encontrado", 404);
+//   }
 
-  if (!user.isAdm) {
-    throw new AppError(
-      "Acesso negado (condição de Administrador necessária)",
-      401
-    );
-  }
+//   if (!user.isAdm) {
+//     throw new AppError(
+//       "Acesso negado (condição de Administrador necessária)",
+//       401
+//     );
+//   }
 
-  return next();
-};
+//   return next();
+// };
 
-export default checkAdmCheckMiddleware;
+// export default checkAdmCheckMiddleware;
