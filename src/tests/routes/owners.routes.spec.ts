263eed8899eea1ccc6cc96efb0b6f1da0bca545a
Andre Junqueira <andrejmelgaco@gmail.com>
2022-05-24 16:34:47 -0300

    fix: refatoracao teste rota owner

--- src/tests/routes/owners.routes.spec.ts
+++ src/tests/routes/owners.routes.spec.ts
@@ -2,15 +2,14 @@ import { DataSource } from "typeorm";
 import { AppDataSource } from "../../data-source";
 import request from "supertest";
 import app from "../../app";
-
 import { v4 as uuid } from "uuid";
 import { User } from "../../entities/users.entity";
 import * as bcript from "bcryptjs";
-import { Owner } from "../../entities/owners.entity";
-// jest.mock("uuid");
+
 describe("Teste para validação do tutor", () => {
   let connection: DataSource;
-
+  let token: string;
+  let owner_id: string;
   beforeAll(async () => {
     await AppDataSource.initialize()
       .then((res) => (connection = res))
@@ -24,12 +23,15 @@ describe("Teste para validação do tutor", () => {
       isAdm: true,
     });
     await userRepository.save(user);
+    const loginResponse = await request(app).post("/sessions").send({
+      name: "admin",
+      password: "admin",
+    });
+    token = loginResponse.body.token;
   });
-
   afterAll(async () => {
     await connection.destroy();
   });
-
   test("Deve retornar que não é possível a criação devido a falta de token", async () => {
     const name = "nome";
     const email = "email@mail.com";
@@ -45,27 +47,22 @@ describe("Teste para validação do tutor", () => {
       message: "Token de autenticação inexistente",
     });
   });
-
   test("Deve ser possível criar um tutor", async () => {
     const name = "nome";
     const email = "email@mail.com";
     const address = "endereço";
     const phone_number = "123456789";
     const ownerData = { name, email, address, phone_number };
-    // const uuidSpy = jest.spyOn(uuid, "v4");
-    // uuidSpy.mockReturnValue("some-uuid");
-    const loginResponse = await request(app).post("/sessions").send({
-      name: "admin",
-      password: "admin",
-    });
-    const token = loginResponse.body.token;
+
     const response = await request(app)
       .post("/owners")
       .set("Authorization", `Bearer ${token}`)
       .send(ownerData);
 
+    owner_id = response.body.id;
+
     expect(response.status).toBe(201);
-    // expect(uuidSpy).toHaveBeenCalled();
+
     expect(response.body.id).toBeDefined();
   });
   test("Não deve ser possível criar um tutor com mesmo email", async () => {
@@ -73,17 +70,7 @@ describe("Teste para validação do tutor", () => {
     const email = "email@mail.com";
     const address = "endereço";
     const phone_number = "123456789";
-    const ownerData = { name, email, address, phone_number };
     const sOwnerData = { name, email, address, phone_number };
-    const loginResponse = await request(app).post("/sessions").send({
-      name: "admin",
-      password: "admin",
-    });
-    const token = loginResponse.body.token;
-    const createResponse = await request(app)
-      .post("/owners")
-      .set("Authorization", `Bearer ${token}`)
-      .send(ownerData);
 
     const response = await request(app)
       .post("/owners")
@@ -91,25 +78,26 @@ describe("Teste para validação do tutor", () => {
       .send(sOwnerData);
 
     expect(response.status).toBe(409);
-    // expect(uuidSpy).toHaveBeenCalled();
+
     expect(response.body.status).toBeDefined();
   });
-  test("Deve ser possível atualizar um tutor", async () => {
-    const loginResponse = await request(app).post("/sessions").send({
-      name: "admin",
-      password: "admin",
+  test("Não deve ser possível atualizar um tutor por falta de token", async () => {
+    const response = await request(app).patch(`/owners/${owner_id}`).send({
+      name: "nome",
+      email: "email2@mail.com",
+      adress: "endereco 2",
+      phone_number: "2345678901",
     });
-    const token = loginResponse.body.token;
-    const owners = await request(app)
-      .get("/owners")
-      .set("Authorization", `Bearer ${token}`);
-    const ownerList = owners.body;
+    expect(response.status).toBe(404);
 
-    const owner = ownerList.find((item: any) => item.name === "nome");
-
-    const id = owner.id;
+    expect(response.body).toEqual({
+      status: "error",
+      message: "Token de autenticação inexistente",
+    });
+  });
+  test("Deve ser possível atualizar um tutor", async () => {
     const response = await request(app)
-      .patch(`/owners/${id}`)
+      .patch(`/owners/${owner_id}`)
       .set("Authorization", `Bearer ${token}`)
       .send({
         name: "nome",
@@ -120,35 +108,17 @@ describe("Teste para validação do tutor", () => {
 
     expect(response.status).toBe(200);
   });
-  test("Não deve ser possível atualizar um tutor por falta de token", async () => {
-    const loginResponse = await request(app).post("/sessions").send({
-      name: "admin",
-      password: "admin",
-    });
-    const token = loginResponse.body.token;
-    const owners = await request(app)
-      .get("/owners")
-      .set("Authorization", `Bearer ${token}`);
-    const ownerList = owners.body;
-    const owner = ownerList.find((item: any) => item.name === "nome");
-
-    const id = owner.id;
-
-    const response = await request(app).patch(`/owners/${id}`).send({
-      name: "nome",
-      email: "email2@mail.com",
-      adress: "endereco 2",
-      phone_number: "2345678901",
-    });
+  test("Não deve ser possível listar os tutores por falta de token", async () => {
+    const response = await request(app).get("/owners").send();
 
     expect(response.status).toBe(404);
+
+    expect(response.body).toEqual({
+      status: "error",
+      message: "Token de autenticação inexistente",
+    });
   });
   test("Deve ser possível listar os tutores", async () => {
-    const loginResponse = await request(app).post("/sessions").send({
-      name: "admin",
-      password: "admin",
-    });
-    const token = loginResponse.body.token;
     const response = await request(app)
       .get("/owners")
       .set("Authorization", `Bearer ${token}`)
@@ -157,13 +127,21 @@ describe("Teste para validação do tutor", () => {
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty("map");
   });
-  test("Não deve ser possível deletar um tutor por id errado", async () => {
-    const loginResponse = await request(app).post("/sessions").send({
-      name: "admin",
-      password: "admin",
+  test("Não deve ser possível deletar um tutor por falta de token", async () => {
+    const response = await request(app).delete(`/owners/${owner_id}`).send({
+      name: "nome",
+      email: "email2@mail.com",
+      adress: "endereco 2",
+      phone_number: "2345678901",
     });
-    const token = loginResponse.body.token;
+    expect(response.status).toBe(404);
 
+    expect(response.body).toEqual({
+      status: "error",
+      message: "Token de autenticação inexistente",
+    });
+  });
+  test("Não deve ser possível deletar um tutor por id errado", async () => {
     const id = uuid();
 
     const response = await request(app)
@@ -172,24 +150,14 @@ describe("Teste para validação do tutor", () => {
       .send();
 
     expect(response.status).toBe(404);
+    expect(response.body).toEqual({
+      status: "error",
+      message: "Tutor não encontrado",
+    });
   });
   test("Deve ser possível deletar um tutor", async () => {
-    const loginResponse = await request(app).post("/sessions").send({
-      name: "admin",
-      password: "admin",
-    });
-    const token = loginResponse.body.token;
-    const owners = await request(app)
-      .get("/owners")
-      .set("Authorization", `Bearer ${token}`);
-    const ownerList = owners.body;
-
-    const owner = ownerList.find((item: any) => item.name === "nome");
-
-    const id = owner.id;
-
     const response = await request(app)
-      .delete(`/owners/${id}`)
+      .delete(`/owners/${owner_id}`)
       .set("Authorization", `Bearer ${token}`);
 
     expect(response.status).toBe(204);
